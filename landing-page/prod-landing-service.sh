805c4b9d45a8a6c8761ae7852dfe4c6d403e3e47
rafiqul <rafiqul@it.damcorp.id>
2021-11-06 12:41:19 +0700

    Fixing type network

--- landing-page/prod-landing-service.sh
+++ landing-page/prod-landing-service.sh
@@ -1,2 +1,2 @@
 #!bin/bash
-kubectl expose deployment landing-page --type=NodePort --port=80 --namespace=production
+kubectl expose deployment landing-page --type=ClusterIP --port=80 --target-port=8080 --namespace=production
